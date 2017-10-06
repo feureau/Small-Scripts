3f56177fdc677d4990dadfc6cd6e0ebe3bf1679f
Feureau-PC <feureau@gmail.com>
2017-10-07 01:04:09 +0700

    Centered to Bandung

--- Google Places API/scripts.js
+++ Google Places API/scripts.js
@@ -5,7 +5,7 @@ function initMap(){
     // Map options
     var options = {
       zoom:8,
-      center:{lat:42.3601,lng:-71.0589}
+      center:{lat:-6.932243417891073,lng:107.63031005859375} 
     }
 
     // New map
