b3b525bc15f0612191dd4af84d4a51ea2da22366
feureau <feureau@gmail.com>
2024-05-02 11:25:23 +0700

    font fix

---
+++ DaVinci Resolve/Wesley_Knapp_HDR_Tools/HDR_MetaJECTOR_v2-8_ACES - DD_NoConfirm - multiple files - noCube.bat
@@ -0,0 +1,20 @@
+COLOR 0C
+
+FOR %%A IN (%*) DO (
+    REM Now your batch file handles %%A instead of %1
+    REM No need to use SHIFT anymore.
+    ECHO %%A
+    mkvmerge.exe -o %%A_hdr.mp4 --colour-matrix 0:9 --colour-range 0:1 --colour-transfer-characteristics 0:16 --colour-primaries 0:9  --max-content-light 0:1000 --max-frame-light 0:300 --max-luminance 0:1000 --min-luminance 0:0.01 --chromaticity-coordinates 0:0.68,0.32,0.265,0.690,0.15,0.06 --white-colour-coordinates 0:0.3127,0.3290 %%A
+    mkvinfo.exe %%A_hdr.mp4
+
+)
+
+REM mkvmerge.exe -o %1_cube_ACES.mp4  --no-chapters --colour-matrix 0:9 --colour-range 0:1 --colour-transfer-characteristics 0:16 --colour-primaries 0:9 --max-content-light 0:1000 --max-frame-light 0:1000 --max-luminance 0:1000 --min-luminance 0:0.01 --chromaticity-coordinates 0:0.68,0.32,0.265,0.690,0.15,0.06 --white-colour-coordinates 0:0.3127,0.3290 --attachment-mime-type application/x-cube --attach-file C:\Users\Feureau\Videos\Wesley_Knapp_HDR_Tools\ACES_Rec2020-ST2084_to_Rec709.cube %1
+
+
+COLOR 0A
+ECHO.
+ECHO Processing complete!  See file specs above.
+ECHO Press Any Key To Exit...
+::PAUSE >nul
+EXIT
