e7f9b812a14380869a130e5bc48c4e4c2e10077d
feureau <feureau@gmail.com>
2024-05-02 17:53:26 +0700

    fixed

--- Windows Batch scripts/FFMPEG batch converters/Convert HDR double height.bat
+++ Windows Batch scripts/FFMPEG batch converters/Convert HDR double height.bat
@@ -5,12 +5,13 @@ FOR %%A IN (%*) DO (
     ECHO %%A
 
 
-    ffmpeg -hwaccel cuda -i %%A -c:v hevc_nvenc -cq:v 16  -b_ref_mode 0 -c:a copy -map 0 -movflags use_metadata_tags -map_metadata 0 -preset fast -tune hq -lookahead_level auto -profile:v main10 -spatial-aq 1 -vf "zscale=-2:(ih*2):filter=lanczos:min=input:m=input:tin=input:t=input:pin=input:p=input,format=yuv420p10le,drawtext=fontfile=FuturaExtraBlack.TTF:text='Subscribe!':fontcolor=black:fontsize=(h/20):box=1:boxcolor=yellow@0.5:boxborderw=15:enable='between(t,0,-5):x=(w-text_w)/2:y=((h/3*2)-th)'" %%A.8K.HDR.mov 
+    ffmpeg  -y -init_hw_device cuda=cuda -filter_hw_device cuda -hwaccel cuda -hwaccel_output_format cuda -i %%A -c:v hevc_nvenc -cq:v 16  -b_ref_mode 0 -c:a copy -map 0 -movflags use_metadata_tags -map_metadata 0 -preset fast -tune hq -lookahead_level auto -profile:v main10 -spatial-aq 1 -filter_complex "[0:v]hwdownload,format=p010le,zscale=-2:(ih*2):filter=lanczos:min=input:m=input:tin=input:t=input:pin=input:p=input,drawtext=fontfile=FuturaExtraBlack.TTF:text='Subscribe!':fontcolor=black:fontsize=(h/20):box=1:boxcolor=yellow@0.5:boxborderw=15:enable='between(t,0,-5):x=(w-text_w)/2:y=((h/3*2)-th)',hwupload_cuda" %%A.8K.HDR.mov 
 
 
-    mkvmerge.exe -o %%A.8K.HDR.mov_cube_ACES.mp4 --colour-matrix 0:9 --colour-range 0:1 --colour-transfer-characteristics 0:16 --colour-primaries 0:9  --max-content-light 0:1000 --max-frame-light 0:300 --max-luminance 0:1000 --min-luminance 0:0.01 --chromaticity-coordinates 0:0.68,0.32,0.265,0.690,0.15,0.06 --white-colour-coordinates 0:0.3127,0.3290 %%A.8K.HDR.mov 
+    mkvmerge.exe -o %%A.2x.HDR.mkv --colour-matrix 0:9 --colour-range 0:1 --colour-transfer-characteristics 0:16 --colour-primaries 0:9  --max-content-light 0:1000 --max-frame-light 0:300 --max-luminance 0:1000 --min-luminance 0:0.01 --chromaticity-coordinates 0:0.68,0.32,0.265,0.690,0.15,0.06 --white-colour-coordinates 0:0.3127,0.3290 %%A.8K.HDR.mov 
 
-    mkvinfo.exe %%A.8K.HDR.mov_cube_ACES.mp4
+    del %%A.8K.HDR.mov
+    mkvinfo.exe %%A.2x.HDR.mkv
 
 )
 
---
+++ Windows Batch scripts/Overwatch/Set of bats/OW split to vert - 8k HDR - GPU.bat
@@ -0,0 +1,34 @@
+COLOR 0C
+
+::set "your_dir=%cd%"     
+::pushd %cd%
+::cd %your_dir%
+
+FOR %%A IN (*.mp4) DO (
+    ::REM Now your batch file handles %%A instead of %1
+    ::REM No need to use SHIFT anymore.
+    ECHO "%%A"
+
+    ffmpeg -ss 00:05.39 -to 00:18.51 -accurate_seek -y -init_hw_device cuda=cuda -filter_hw_device cuda -hwaccel cuda -hwaccel_output_format cuda -i %%A -c:v hevc_nvenc -cq:v 16  -b_ref_mode 0 -c:a copy -map 0 -movflags use_metadata_tags -map_metadata 0 -preset fast -tune hq -lookahead_level auto -profile:v main10 -spatial-aq 1 -filter_complex  "[0:v]hwdownload,format=nv12,crop=(3/4*ih):ih:(iw-(3/4*ih))/2:0,zscale=-2:7680:filter=lanczos:min=input:m=input:tin=input:t=input:pin=input:p=input,format=yuv420p10le,drawtext=fontfile=FuturaExtraBlack.TTF:text='Subscribe!':fontcolor=black:fontsize=(h/20):box=1:boxcolor=yellow@0.5:boxborderw=15:enable='between(t,10,15):x=(w-text_w)/2:y=((h/3*2)-th)',hwupload_cuda" "%%A_shorts-8k.mov"
+
+    mkvmerge.exe -o %%A_shorts-8k_mov_HDR.mkv --colour-matrix 0:9 --colour-range 0:1 --colour-transfer-characteristics 0:16 --colour-primaries 0:9  --max-content-light 0:1000 --max-frame-light 0:300 --max-luminance 0:1000 --min-luminance 0:0.01 --chromaticity-coordinates 0:0.68,0.32,0.265,0.690,0.15,0.06 --white-colour-coordinates 0:0.3127,0.3290 %%A_shorts-8k.mov
+
+
+
+     ECHO "%%A"
+     del %%A_shorts-8k.mov
+     move "%%A_shorts-8k_mov_HDR.mkv" "%cd%\vert split"
+     popd
+)
+
+::    mkvinfo.exe %%A_shorts-8k_mov_HDR.mkv
+:: horz scale: iw*(7680/iw):ih*(4320/ih)
+:: vert scale: iw*(4320/iw):ih*(5760/ih)
+::libx265
+::x=(w-text_w)/2:y=((h/3)-th)
+::COLOR 0A
+::ECHO.
+::ECHO Processing complete!  See file specs above.
+::ECHO Press Any Key To Exit...
+::PAUSE >nul
+::EXIT
