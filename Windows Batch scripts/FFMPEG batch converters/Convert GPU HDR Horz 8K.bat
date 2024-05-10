aad5a5794d9a60bed345452da284f04c5e742991
feureau <feureau@gmail.com>
2024-05-10 21:38:37 +0700

    update

---
+++ Windows Batch scripts/FFMPEG batch converters/Convert GPU HDR Horz 8K.bat
@@ -0,0 +1,24 @@
+@echo off
+COLOR 0C
+
+FOR %%A IN (%*) DO (
+    ECHO %%A
+
+
+    ffmpeg -y -hwaccel cuda -i %%A -c:v hevc_nvenc -cq:v 16  -b_ref_mode 0 -c:a copy -map 0 -preset fast -tune hq -lookahead_level auto -profile:v main10 -spatial-aq 1 -vf "scale=-2:4320:flags=lanczos" -movflags use_metadata_tags -map_metadata 0 -pix_fmt yuv420p10le -color_primaries 9 -color_trc 16 -colorspace 9 -color_range 1 %%A.Horz.8K.HDR.mov
+
+    mkvmerge.exe -o %%A.Horz.8K.HDR.mkv --colour-matrix 0:9 --colour-range 0:1 --colour-transfer-characteristics 0:16 --colour-primaries 0:9  --max-content-light 0:1000 --max-frame-light 0:300 --max-luminance 0:1000 --min-luminance 0:0.01 --chromaticity-coordinates 0:0.68,0.32,0.265,0.690,0.15,0.06 --white-colour-coordinates 0:0.3127,0.3290 %%A.Horz.8K.HDR.mov
+    del %%A.Horz.8K.HDR.mov
+    mkvinfo.exe %%A.Horz.8K.HDR.mkv
+)
+
+
+::scale=iw*(7680/iw):ih*(4320/ih)
+::libx265
+::x=(w-text_w)/2:y=((h/3)-th)
+COLOR 0A
+::ECHO.
+::ECHO Processing complete!  See file specs above.
+::ECHO Press Any Key To Exit...
+PAUSE >nul
+EXIT
