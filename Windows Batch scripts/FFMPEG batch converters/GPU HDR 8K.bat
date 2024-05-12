a6ce503b6e8f510f3374ea7f8c879bea4d7bb51e
feureau <feureau@gmail.com>
2024-05-13 02:54:42 +0700

    updated setting

--- Windows Batch scripts/FFMPEG batch converters/GPU HDR 8K.bat
+++ Windows Batch scripts/FFMPEG batch converters/GPU HDR 8K.bat
@@ -5,10 +5,10 @@ FOR %%A IN (%*) DO (
     ECHO %%A
 
 
-    ffmpeg -y -hwaccel cuda -i %%A -c:v hevc_nvenc -cq:v 16  -b_ref_mode 0 -c:a copy -map 0 -preset fast -tune hq -lookahead_level auto -profile:v main10 -spatial-aq 1 -vf "scale=7680:7680:force_original_aspect_ratio=decrease:flags=lanczos" -movflags use_metadata_tags -map_metadata 0 -pix_fmt p010le %%A.Horz.8K.HDR.mov
+    ffmpeg -y -hwaccel cuda -i %%A -c:v hevc_nvenc -cq:v 16  -b_ref_mode 0 -c:a copy -map 0  -preset p1 -tune ull -lookahead_level auto -profile:v main10 -spatial-aq 1 -vf "scale=7680:7680:force_original_aspect_ratio=decrease:flags=lanczos" -movflags use_metadata_tags -map_metadata 0 -pix_fmt p010le %%A.Horz.8K.HDR.mov
 
 
-    mkvmerge.exe -o %%A.Horz.8K.HDR.ACES.mov --no-chapters --colour-matrix 0:9 --colour-range 0:1 --colour-transfer-characteristics 0:16 --colour-primaries 0:9  --max-content-light 0:1000 --max-frame-light 0:300 --max-luminance 0:1000 --min-luminance 0:0.01 --chromaticity-coordinates 0:0.68,0.32,0.265,0.690,0.15,0.06 --white-colour-coordinates 0:0.3127,0.3290 --attachment-mime-type application/x-cube --attach-file "C:\Users\Feureau\Desktop\ACES_Rec2020-ST2084_to_Rec709.cube" %%A.Horz.8K.HDR.mov
+    mkvmerge.exe -o %%A.Horz.8K.HDR.ACES.mov --no-chapters --colour-matrix 0:9 --colour-range 0:1 --colour-transfer-characteristics 0:16 --colour-primaries 0:9  --max-content-light 0:1000 --max-frame-light 0:300 --max-luminance 0:1000 --min-luminance 0:0.0001 --chromaticity-coordinates 0:0.7080,0.2920,0.1700,0.7970,0.1310,0.0460 --white-colour-coordinates 0:0.3127,0.3290 %%A.Horz.8K.HDR.mov
     del %%A.Horz.8K.HDR.mov
     mkvinfo.exe %%A.Horz.8K.HDR.ACES.mov
 )
--- Windows Batch scripts/FFMPEG batch converters/GPU HDR Sub60s 8K.bat
+++ Windows Batch scripts/FFMPEG batch converters/GPU HDR Sub60s 8K.bat
@@ -3,12 +3,15 @@ COLOR 0C
 
 FOR %%A IN (%*) DO (
     ECHO %%A
-    ffmpeg -ss 00:00.00 -to 00:59.984 -accurate_seek -y -hwaccel cuda -i %%A -c:v hevc_nvenc -cq:v 16 -b_ref_mode 0 -c:a copy -map 0 -movflags use_metadata_tags -map_metadata 0 -preset fast -tune hq -lookahead_level auto -profile:v main10 -spatial-aq 1 -vf "drawtext=fontfile=E\\:Small-Scripts/Windows Batch scripts/FFMPEG batch converters/FuturaExtraBlack.TTF:text='Subscribe!':fontcolor=black:fontsize=(h/20):box=1:boxcolor=yellow@0.5:boxborderw=15:enable='between(t,1,6):x=(w-text_w)/2:y=((h/3*2)-th)',scale=7680:7680:force_original_aspect_ratio=decrease:flags=lanczos" -pix_fmt p010le %%A.Vert.8K.HDR.mov 
+    ffmpeg -ss 00:00.00 -to 00:59.984 -accurate_seek -y -hwaccel cuda -i %%A -c:v hevc_nvenc -cq:v 16 -b_ref_mode 0 -c:a copy -map 0  -preset p1 -tune ull -lookahead_level auto -profile:v main10 -spatial-aq 1 -pix_fmt p010le -vf "drawtext=fontfile=E\\:Small-Scripts/Windows Batch scripts/FFMPEG batch converters/FuturaExtraBlack.TTF:text='Subscribe!':fontcolor=black:fontsize=(h/20):box=1:boxcolor=yellow@0.5:boxborderw=15:enable='between(t,1,6):x=(w-text_w)/2:y=((h/3*2)-th)',scale=7680:7680:force_original_aspect_ratio=decrease:flags=lanczos" %%A.Vert.8K.HDR.mov 
 
-    mkvmerge.exe -o %%A.Vert.8K.HDR.ACES.mov --no-chapters --colour-matrix 0:9 --colour-range 0:1 --colour-transfer-characteristics 0:16 --colour-primaries 0:9  --max-content-light 0:1000 --max-frame-light 0:300 --max-luminance 0:1000 --min-luminance 0:0.01 --chromaticity-coordinates 0:0.68,0.32,0.265,0.690,0.15,0.06 --white-colour-coordinates 0:0.3127,0.3290 --attachment-mime-type application/x-cube --attach-file "C:\Users\Feureau\Desktop\ACES_Rec2020-ST2084_to_Rec709.cube" %%A.Vert.8K.HDR.mov 
+REM    mkvmerge.exe -o %%A.Vert.8K.HDR.ACES.mov --no-chapters --colour-matrix 0:9 --colour-range 0:1 --colour-transfer-characteristics 0:16 --colour-primaries 0:9  --max-content-light 0:1000 --max-frame-light 0:300 --max-luminance 0:1000 --min-luminance 0:0.0001 --chromaticity-coordinates 0:0.7080,0.2920,0.1700,0.7970,0.1310,0.0460 --white-colour-coordinates 0:0.3127,0.3290 %%A.Vert.8K.HDR.mov 
 
-    del %%A.Vert.8K.HDR.mov 
-    mkvinfo.exe %%A.Vert.8K.HDR.ACES.mov
+REM    del %%A.Vert.8K.HDR.mov 
+REM    mkvinfo.exe %%A.Vert.8K.HDR.ACES.mov
 )
+
+REM    -movflags use_metadata_tags -map_metadata 0 
+REM    -color_primaries 9 -color_trc 16 -colorspace 9 -color_range 1
 PAUSE >nul
 EXIT
