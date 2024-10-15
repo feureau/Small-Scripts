3d8896dc81d449b279460c5ecfdf849b60ff9718
feureau <feureau@gmail.com>
2024-10-16 03:02:00 +0700

    update

--- Windows Batch scripts/NVEncc batch converters/NV HDSDR-HDHDR.bat
+++ Windows Batch scripts/NVEncc batch converters/NV HDSDR-HDHDR.bat
@@ -4,7 +4,7 @@ COLOR 0C
 FOR %%A IN (%*) DO (
     ECHO %%A
 
-NVEncC64 --avhw --codec hevc --tier high --profile main444 --output-csp yuv444 --qvbr 0 --preset p5 --output-depth 10 --multipass 2pass-full --lookahead 32 --nonrefp --aq --aq-temporal --aq-strength 0 --transfer auto --audio-copy --chapter-copy --key-on-chapter --metadata copy --vpp-nvvfx-artifact-reduction mode=0 --vpp-ngx-truehdr maxluminance=1000 --colormatrix bt2020nc --colorprim bt2020 --transfer smpte2084 -i %%A -o %%A_HDR_.mkv 
+NVEncC64 --codec hevc --tier high --profile main444 --output-csp yuv444 --qvbr 0 --preset p7 --output-depth 10 --multipass 2pass-full --lookahead 32 --nonrefp --aq --aq-temporal --aq-strength 0 --transfer auto --audio-copy --chapter-copy --key-on-chapter --metadata copy --vpp-nvvfx-artifact-reduction mode=0 --vpp-ngx-truehdr maxluminance=1000 --colormatrix bt2020nc --colorprim bt2020 --transfer smpte2084 -i %%A -o %%A_HDR_.mkv 
 
 
     mkdir HDR
--- Windows Batch scripts/NVEncc batch converters/NV SDR-HDR.bat
+++ Windows Batch scripts/NVEncc batch converters/NV SDR-HDR.bat
@@ -4,13 +4,16 @@ COLOR 0C
 FOR %%A IN (%*) DO (
     ECHO %%A
 
-start /b /belownormal /wait ffmpeg -ss 00:00.00 -to 00:18.51 -accurate_seek -y -init_hw_device cuda=gpu:0 -filter_hw_device gpu -hwaccel cuvid -hwaccel_output_format cuda -i %%A -c:v hevc_nvenc -cq:v 8 -c:a copy -c:s copy -map 0 -map_metadata 0 -preset p4 -tune hq -profile:v main10 -spatial-aq 1 -temporal-aq 1 %%A_split.mkv
+NVEncC64 --codec hevc --tier high --profile main444 --output-csp yuv444 --qvbr 0 --preset p5 --output-depth 10 --multipass 2pass-full --lookahead 32 --nonrefp --aq --aq-temporal --aq-strength 0 --transfer auto --audio-copy --chapter-copy --key-on-chapter --metadata copy --vpp-ngx-truehdr maxluminance=1000 --colormatrix bt2020nc --colorprim bt2020 --transfer smpte2084 -i %%A -o %%A_HDR_.mkv 
 
-    mkdir split
-    move %%A_split.mkv split\
+
+    mkdir HDR
+    move %%A_HDR_.mkv  HDR\
 )
 
-REM -y -init_hw_device cuda=gpu:0 -filter_hw_device gpu -hwaccel cuvid -hwaccel_output_format cuda
+REM --output-csp yuv444 
+
+REM 
 REM format=p010le,zscale=-2:7680:filter=lanczos:min=input:m=input:tin=input:t=input:pin=input:p=input,format=yuv420p10le,
 
 ::thumbnail,scale='if(gt(iw,ih),7680,trunc(oh*a/2)*2)':'if(gt(iw,ih),trunc(ow/a/2)*2,7680)'
